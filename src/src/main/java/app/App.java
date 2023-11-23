b7c7b13beb96d101964443a822b05432c20117ef
osawa-koki <koki@osawa.dev>
2023-11-23 22:44:48 +0900

    リファクタリング。

--- src/src/main/java/app/App.java
+++ src/src/main/java/app/App.java
@@ -1,4 +1,4 @@
-package helloworld;
+package app;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/src/test/java/app/AppTest.java
+++ src/src/test/java/app/AppTest.java
@@ -1,4 +1,4 @@
-package helloworld;
+package app;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;
